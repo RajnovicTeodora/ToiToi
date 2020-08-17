acd4ab49e97fa0e623c7472be1a801ab4f13c481
Isidora <69584865+isidorasavic@users.noreply.github.com>
2020-08-17 18:16:10 +0200

    mainFrame

--- PlatformForBeginnerCooks/src/view/Application.java
+++ PlatformForBeginnerCooks/src/view/Application.java
@@ -1,10 +1,13 @@
-package application;
+package view;
 
 import controller.ToiToiController;
 
 public class Application {
 	
 	public static void main(String args[]) {
+		
+		MainFrame mf = new MainFrame();
+        mf.setVisible(true);
 		
 		ToiToiController toiToiController = new ToiToiController();
 		toiToiController.writteData();
